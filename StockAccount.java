70974caabc2d2510c8f0ef366cc1524c6a1c9207
lumian <lumianlq@gmail.com>
2021-12-06 23:09:25 +0800

    part of the stock account done

--- StockAccount.java
+++ StockAccount.java
@@ -1,4 +1,47 @@
+import java.util.ArrayList;
+
 public class StockAccount extends Account {
+    private ArrayList<Stock> stocks;
+
+    public StockAccount() {
+        super();
+        this.moneyList = new ArrayList<Money>();
+        this.curr = new Currency();
+        constructMoneyList();
+        this.stocks = new ArrayList<Stock>();
+    }
+
+    public void addStock(Stock stock) {
+        this.stocks.add(stock);
+    }
+
+    public Stock getStock(int index) {
+        return this.stocks.get(index);
+    }
+
+    public void removeStock(int index) {
+        this.stocks.remove(index);
+    }
+
+    public void buyStock(int amount, Stock stock) {
+        convertAllToUSD();
+        float totalPrice = stock.getPrice() * amount;
+        if (totalPrice > this.getBalance()) {
+            System.out.println("Incifficient balance");
+        } else {
+            this.moneyList.get(0).subtractMoneyAmount(totalPrice);
+            for (int i = 0; i < amount; i++) {
+                addStock(stock);
+            }
+        }
+    }
+
+    public void printStock() {
+        for (Stock s : this.stocks) {
+            System.out.println(
+                    String.format("Name %s, code %s, current price %.2f", s.getName(), s.getCode(), s.getPrice()));
+        }
+    }
 
     @Override
     public String toString() {
