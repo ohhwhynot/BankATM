3764b6536dda1fd56ab017f1ba552c65c92f5754
Xudong Gao <xdgao@bu.edu>
2021-12-11 17:05:54 +0800

    add confirm and reset

--- DepositWithdrawScreen.java
+++ DepositWithdrawScreen.java
@@ -25,6 +25,7 @@ public class DepositWithdrawScreen extends JFrame {
                 return result;
         }
 
+
         private void accountActionPerformed(ActionEvent e) {
                 new AccountInfoScreen().setVisible(true);
                 // TODO add your code here
@@ -41,6 +42,15 @@ public class DepositWithdrawScreen extends JFrame {
                 // TODO add your code here
         }
 
+        private void resetActionPerformed(ActionEvent e) {
+            textField1.setText(null);
+            // TODO add your code here
+        }
+
+        private void confirmActionPerformed(ActionEvent e) {
+            // TODO add your code here
+        }
+
         private void initComponents() {
                 // JFormDesigner - Component initialization - DO NOT MODIFY
                 // //GEN-BEGIN:initComponents
@@ -55,103 +65,98 @@ public class DepositWithdrawScreen extends JFrame {
                 account = new JButton();
                 switchAcc = new JButton();
 
-                // ======== this ========
+                //======== this ========
                 var contentPane = getContentPane();
 
-                // ---- label1 ----
+                //---- label1 ----
                 label1.setText("Please input the amount you want to deposit");
 
-                // ---- comboBox1 ----
-                comboBox1.setModel(new DefaultComboBoxModel<>(new String[] { "USD", "EUR", "CNY", "JPY" }));
+                //---- comboBox1 ----
+                comboBox1.setModel(new DefaultComboBoxModel<>(new String[] {
+                    "USD",
+                    "EUR",
+                    "CNY",
+                    "JPY"
+                }));
 
-                // ---- reset ----
+                //---- reset ----
                 reset.setText("Reset");
+                reset.addActionListener(e -> resetActionPerformed(e));
 
-                // ---- confirm ----
+                //---- confirm ----
                 confirm.setText("Confirm");
+                confirm.addActionListener(e -> {
+			confirmActionPerformed(e);
+			confirmActionPerformed(e);
+		});
 
-                // ---- Return ----
+                //---- Return ----
                 Return.setText("Return");
                 Return.addActionListener(e -> ReturnActionPerformed(e));
 
-                // ---- account ----
+                //---- account ----
                 account.setText("Account Information");
                 account.addActionListener(e -> accountActionPerformed(e));
 
-                // ---- switchAcc ----
+                //---- switchAcc ----
                 switchAcc.setText("Switch Account");
                 switchAcc.addActionListener(e -> switchAccActionPerformed(e));
 
                 GroupLayout contentPaneLayout = new GroupLayout(contentPane);
                 contentPane.setLayout(contentPaneLayout);
-                contentPaneLayout.setHorizontalGroup(contentPaneLayout.createParallelGroup().addGroup(contentPaneLayout
-                                .createSequentialGroup()
-                                .addGroup(contentPaneLayout.createParallelGroup().addGroup(contentPaneLayout
-                                                .createSequentialGroup().addGap(136, 136, 136)
-                                                .addGroup(contentPaneLayout.createParallelGroup().addGroup(
-                                                                contentPaneLayout.createSequentialGroup().addComponent(
-                                                                                reset, GroupLayout.PREFERRED_SIZE, 130,
-                                                                                GroupLayout.PREFERRED_SIZE)
-                                                                                .addGap(34, 34, 34)
-                                                                                .addComponent(confirm,
-                                                                                                GroupLayout.PREFERRED_SIZE,
-                                                                                                130,
-                                                                                                GroupLayout.PREFERRED_SIZE))
-                                                                .addGroup(contentPaneLayout.createSequentialGroup()
-                                                                                .addComponent(comboBox1,
-                                                                                                GroupLayout.PREFERRED_SIZE,
-                                                                                                110,
-                                                                                                GroupLayout.PREFERRED_SIZE)
-                                                                                .addPreferredGap(
-                                                                                                LayoutStyle.ComponentPlacement.RELATED)
-                                                                                .addComponent(textField1,
-                                                                                                GroupLayout.PREFERRED_SIZE,
-                                                                                                178,
-                                                                                                GroupLayout.PREFERRED_SIZE))))
-                                                .addGroup(contentPaneLayout.createSequentialGroup().addContainerGap()
-                                                                .addComponent(Return, GroupLayout.PREFERRED_SIZE, 76,
-                                                                                GroupLayout.PREFERRED_SIZE)
-                                                                .addGap(102, 102, 102).addComponent(account)
-                                                                .addGap(47, 47, 47).addComponent(switchAcc))
-                                                .addGroup(contentPaneLayout.createSequentialGroup().addGap(96, 96, 96)
-                                                                .addGroup(contentPaneLayout.createParallelGroup()
-                                                                                .addComponent(separator1,
-                                                                                                GroupLayout.PREFERRED_SIZE,
-                                                                                                395,
-                                                                                                GroupLayout.PREFERRED_SIZE)
-                                                                                .addGroup(contentPaneLayout
-                                                                                                .createSequentialGroup()
-                                                                                                .addGap(18, 18, 18)
-                                                                                                .addComponent(label1,
-                                                                                                                GroupLayout.PREFERRED_SIZE,
-                                                                                                                354,
-                                                                                                                GroupLayout.PREFERRED_SIZE)))))
-                                .addContainerGap(40, Short.MAX_VALUE)));
-                contentPaneLayout.setVerticalGroup(contentPaneLayout.createParallelGroup().addGroup(contentPaneLayout
-                                .createSequentialGroup().addGap(22, 22, 22)
-                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                                .addComponent(switchAcc, GroupLayout.PREFERRED_SIZE, 40,
-                                                                GroupLayout.PREFERRED_SIZE)
-                                                .addComponent(account, GroupLayout.PREFERRED_SIZE, 40,
-                                                                GroupLayout.PREFERRED_SIZE)
-                                                .addComponent(Return, GroupLayout.PREFERRED_SIZE, 40,
-                                                                GroupLayout.PREFERRED_SIZE))
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
-                                .addComponent(separator1, GroupLayout.PREFERRED_SIZE, 10, GroupLayout.PREFERRED_SIZE)
-                                .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED).addComponent(label1)
-                                .addGap(32, 32, 32)
-                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                                .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE,
-                                                                GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                                .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 47,
-                                                                GroupLayout.PREFERRED_SIZE))
-                                .addGap(42, 42, 42)
-                                .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
-                                                .addComponent(confirm, GroupLayout.PREFERRED_SIZE, 40,
-                                                                GroupLayout.PREFERRED_SIZE)
-                                                .addComponent(reset, GroupLayout.PREFERRED_SIZE, 40,
-                                                                GroupLayout.PREFERRED_SIZE))
-                                .addContainerGap(94, Short.MAX_VALUE)));
+                contentPaneLayout.setHorizontalGroup(
+                    contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGroup(contentPaneLayout.createParallelGroup()
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addGap(136, 136, 136)
+                                    .addGroup(contentPaneLayout.createParallelGroup()
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                            .addComponent(reset, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+                                            .addGap(34, 34, 34)
+                                            .addComponent(confirm, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE))
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                            .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, 110, GroupLayout.PREFERRED_SIZE)
+                                            .addPreferredGap(LayoutStyle.ComponentPlacement.RELATED)
+                                            .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 178, GroupLayout.PREFERRED_SIZE))))
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addContainerGap()
+                                    .addComponent(Return, GroupLayout.PREFERRED_SIZE, 76, GroupLayout.PREFERRED_SIZE)
+                                    .addGap(102, 102, 102)
+                                    .addComponent(account)
+                                    .addGap(47, 47, 47)
+                                    .addComponent(switchAcc))
+                                .addGroup(contentPaneLayout.createSequentialGroup()
+                                    .addGap(96, 96, 96)
+                                    .addGroup(contentPaneLayout.createParallelGroup()
+                                        .addComponent(separator1, GroupLayout.PREFERRED_SIZE, 395, GroupLayout.PREFERRED_SIZE)
+                                        .addGroup(contentPaneLayout.createSequentialGroup()
+                                            .addGap(18, 18, 18)
+                                            .addComponent(label1, GroupLayout.PREFERRED_SIZE, 354, GroupLayout.PREFERRED_SIZE)))))
+                            .addContainerGap(40, Short.MAX_VALUE))
+                );
+                contentPaneLayout.setVerticalGroup(
+                    contentPaneLayout.createParallelGroup()
+                        .addGroup(contentPaneLayout.createSequentialGroup()
+                            .addGap(22, 22, 22)
+                            .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(switchAcc, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(account, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(Return, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(separator1, GroupLayout.PREFERRED_SIZE, 10, GroupLayout.PREFERRED_SIZE)
+                            .addPreferredGap(LayoutStyle.ComponentPlacement.UNRELATED)
+                            .addComponent(label1)
+                            .addGap(32, 32, 32)
+                            .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(comboBox1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(textField1, GroupLayout.PREFERRED_SIZE, 47, GroupLayout.PREFERRED_SIZE))
+                            .addGap(42, 42, 42)
+                            .addGroup(contentPaneLayout.createParallelGroup(GroupLayout.Alignment.BASELINE)
+                                .addComponent(confirm, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE)
+                                .addComponent(reset, GroupLayout.PREFERRED_SIZE, 40, GroupLayout.PREFERRED_SIZE))
+                            .addContainerGap(94, Short.MAX_VALUE))
+                );
                 setSize(595, 410);
                 setLocationRelativeTo(getOwner());
                 // JFormDesigner - End of component initialization //GEN-END:initComponents
@@ -169,4 +174,7 @@ public class DepositWithdrawScreen extends JFrame {
         private JButton account;
         private JButton switchAcc;
         // JFormDesigner - End of variables declaration //GEN-END:variables
-}
+
+    }
+
+
